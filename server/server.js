0e6a7b5220ba2a6d1bbbbf079c4198d9dbfd5e03
Roshan Bist <roshanbist@Roshans-MacBook-Pro.local>
2026-02-25 23:20:51 +0100

    updated to deploy in railway

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use("/api/project", projectRoutes);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
