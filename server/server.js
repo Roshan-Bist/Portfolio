120b1c33f2ff27fc3082acc5259e4336b0e93526
Roshan Bist <roshanbist@Roshans-MacBook-Pro.local>
2026-02-25 23:09:46 +0100

    fix: monorepo railway deployment config

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ app.get("*", (req, res) => {
 
 connectDB();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
